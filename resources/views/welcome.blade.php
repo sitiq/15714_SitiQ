550bf6770529b6a8d4dc83c9b589f02bc65919ef
ata23 <suhitaramaa@gmail.com>
2019-11-29 10:17:54 +0700

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rahma Suhita
                 </div>
 
                 <div class="links">
